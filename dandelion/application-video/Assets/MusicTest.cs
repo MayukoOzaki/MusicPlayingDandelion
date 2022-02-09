52ba621d9b3308ccba6e1a95be8131ab12453562
MayukoOzaki <22011077mo@tama.ac.jp>
2022-02-09 20:54:42 +0900

    音を出す(2)

--- dandelion/application-video/Assets/MusicTest.cs
+++ dandelion/application-video/Assets/MusicTest.cs
@@ -32,25 +32,25 @@ public class MusicTest : MonoBehaviour
 
         //▼MIDIデバイスオープン
         MusicTest.midiOutOpen(out hMidiOut, MIDI_MAPPER, IntPtr.Zero, IntPtr.Zero, uint.MinValue);
-        play();
+        //play();
 
-        //uint ch = 0xc0;
-        //uint ToneColor = 0x0;
-        //uint TC_data = (ch << 0) + (ToneColor << 8);
-        //Debug.Log(TC_data.ToString("X"));
-        //midiOutShortMsg(hMidiOut, TC_data);   // 音色を定義 アコースティックピアノ0x0(0)
+        uint ch = 0xc0;
+        uint ToneColor = 0x0;
+        uint TC_data = (ch << 0) + (ToneColor << 8);
+        Debug.Log(TC_data.ToString("X"));
+        midiOutShortMsg(hMidiOut, TC_data);   // 音色を定義 アコースティックピアノ0x0(0)
 
-        //uint on = 0x90;
-        //uint off = 0x80;
-        //uint Pitch = 0x30;
-        //uint Velocity = 0x7f;
-        //uint on_data = (on << 0) + (Pitch << 16) + (Velocity << 24);
-        //uint off_data = (off << 0) + (Pitch << 16) + (Velocity << 24);
-        //Debug.Log(on_data.ToString("X"));
+        uint on = 0x90;
+        uint off = 0x80;
+        uint Pitch = 0x30;
+        uint Velocity = 0x7f;
+        uint on_data = (on << 0) + (Pitch << 8) + (Velocity << 16);
+        uint off_data = (off << 0) + (Pitch << 8) + (Velocity << 16);
+        Debug.Log(on_data.ToString("X"));
 
-        //NotePlayer.midiOutShortMsg(hMidiOut, on_data);
-        //Task.Delay(277).Wait();
-        //midiOutShortMsg(hMidiOut, off_data);
+        NotePlayer.midiOutShortMsg(hMidiOut, on_data);
+        Task.Delay(554).Wait();
+        midiOutShortMsg(hMidiOut, off_data);
 
         //Debug.Log(hMidiOut);
 
