bd28b223f3363eda7a97d54713fd4103b8962633
MayukoOzaki <22011077mo@tama.ac.jp>
2022-02-18 19:02:50 +0900

    データをテキスト保存

--- dandelion/application-video/Assets/MicVolumeSample.cs
+++ dandelion/application-video/Assets/MicVolumeSample.cs
@@ -1,6 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 public class MicVolumeSample : MonoBehaviour
 {
@@ -11,6 +17,12 @@ public class MicVolumeSample : MonoBehaviour
 
     private AudioSource aud;
     private readonly float[] spectrum = new float[256];
+
+    private List<List<string>> datalist = new List<List<string>>();
+    private int count = 0;
+   
+
+
     // Use this for initialization
     void Start()
     {
@@ -23,6 +35,7 @@ public class MicVolumeSample : MonoBehaviour
             aud.clip = Microphone.Start(devName, true, 2, minFreq); // 音の大きさを取るだけなので最小サンプリングで十分
             aud.Play(); //マイクをオーディオソースとして実行(Play)開始
         }
+        CreateFile1();
     }
 
     // Update is called once per frame
@@ -32,15 +45,42 @@ public class MicVolumeSample : MonoBehaviour
         for(int i=0;i<256;i++)
             if(spectrum[i]>0.01f)
             {
-                Debug.Log(i);
-                Debug.Log(spectrum[i]);
+                if  (Input.GetKeyDown(KeyCode.P))
+                {
+                    float fHz = i;
+                    string hz = fHz.ToString();
+                    float fNum = spectrum[i];
+                    string num = fNum.ToString();
+                    datalist[count].Add(hz);
+                    datalist[count].Add(num);
+                }
+                //datalist[count].Add(i);
+                //datalist[count].Add(spectrum[i]);
+
+                //Debug.Log(i);
+                //Debug.Log(spectrum[i]);
+                count += 1;
             }
-           
+
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            int a = datalist.Count;
+
+            string path = Application.persistentDataPath + "/sample1.txt";
+
+            for  (int i = 0; i < a; i++)
+            {
+                string text = datalist[i][0] + " " + datalist[i][1];
+                File.WriteAllText(path, text);
+            }
+                    
+
+        }
 
 
-        //Debug.Log(spectrum);
-        //Debug.Log(m_volumeRate);
-    }
+            //Debug.Log(spectrum);
+            //Debug.Log(m_volumeRate);
+        }
 
     // オーディオが読まれるたびに実行される
     private void OnAudioFilterRead(float[] data, int channels)
@@ -55,5 +95,12 @@ public class MicVolumeSample : MonoBehaviour
         m_volumeRate = Mathf.Clamp01(sum * m_gain / (float)data.Length);
 
         //volumeList.Add(m_volumeRate);
+    }
+
+    private void CreateFile1()
+    {
+        var path = "sample1.txt";
+        var fileStream = File.Create(path);
+        fileStream.Close();
     }
 }
