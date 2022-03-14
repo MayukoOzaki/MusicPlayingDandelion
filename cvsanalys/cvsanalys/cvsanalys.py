5326807b9fba1c3f2bfb21371a136cc2008cdfde
MayukoOzaki <22011077mo@tama.ac.jp>
2022-03-14 13:30:57 +0100

    分析

--- cvsanalys/cvsanalys/cvsanalys.py
+++ cvsanalys/cvsanalys/cvsanalys.py
@@ -1,29 +1,58 @@
 import csv
 
 
-csvfile = open(r"C:\Users\mayuk\source\repos\cvsanalys\SaveData002.csv", 'r')
+csvfile = open(r"C:\Users\mayuk\source\repos\MusicPlayingDandelion\分析用CVSデータ\masukuCVS012.csv", 'r')
 reader = csv.reader(csvfile)
 
-sum=0
-sqsum=0
+print(reader)
+
+#全体の平均(マスクあり)　24.71064217
+
+#全体の平均(マスクなし)　35.97260743
+
+withmask=24.71064217**2
+
+nomask=35.97260743**2
 
 
-
-for row in reader:    
-    print(row)
-
+for row in reader:
+    # print(row)
     frequency = float(row[0])
     quantity = float(row[1])
+    print(frequency)
+
+def analys(read):
+    sum=0
+    sqsum=0
     
+    cou=0
 
-    sum+=quantity
-    sqsum+=quantity**2
+    for row in read:    
+       # print(row)
+        frequency = float(row[0])
+        quantity = float(row[1])
+        sum+=frequency
+        sqsum+=frequency**2
+        cou+=1
 
-    if  frequency==255:
-        break
+    print("できた")
+    avesum=(sum/cou)**2 #合計の平均の２乗
 
-avesum=(sum/256)**2 #合計の平均の２乗
-avesqsum=sqsum/256 #2乗合計の平均
+    avesqsum=sqsum/cou #2乗合計の平均
 
-variance= avesqsum-avesum #分散
-print("variance",variance)
+    print("二乗の合計", sqsum)
+
+    variance= avesqsum-avesum #分散
+    print("variance",variance)
+
+    deviation=variance**0.5
+    print("standerd deviation", deviation)
+
+
+
+
+analys(reader)
+
+
+
+
