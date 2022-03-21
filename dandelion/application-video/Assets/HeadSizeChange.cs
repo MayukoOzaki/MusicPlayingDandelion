06d2fcca9997501f10b07266f80186c7c71064e6
MayukoOzaki <22011077mo@tama.ac.jp>
2022-03-21 17:36:49 +0100

    頭の大きさを変更するスクリプトの作成

---
+++ dandelion/application-video/Assets/HeadSizeChange.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadSizeChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform tf = gameObject.GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
