f40246c4e08e371c4bee478b9ebcf51c882dc287
NaoyaSugiyama1 <21911198ns@tama.ac.jp>
2022-03-22 20:11:18 +0900

    add DandelionModelchange.cs
    たんぽぽとカメラが一定以上近くなったら綿毛部分のモデルを入れ替えるスクリプトの追加

---
+++ dandelion/application-video/Assets/DandelionModels/Scripts/DandelionModelchange.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DandelionModelchange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Camera;
+    public GameObject HeadOutside;
+    public GameObject HeadModel;
+    public float DistanceCamera;
+    Transform Cameratransform;
+    Transform Dandeliontransform;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //カメラの座標
+        Cameratransform = Camera.transform;
+        Vector3 Camerapos = Cameratransform.position;
+
+        //たんぽぽの座標
+        Dandeliontransform = this.transform;
+        Vector3 Dandelionpos = Dandeliontransform.position;
+
+        //カメラとたんぽぽ間の距離
+        DistanceCamera = Dandelionpos.z - Camerapos.z;
+
+        //距離が一定以上近くなったら簡易版のモデルを非表示にして、処理が重いモデルを表示する
+        if (DistanceCamera < 30.0f)
+        {
+            HeadOutside.SetActive(false);
+            HeadModel.SetActive(true);
+        }
+    }
+}
