7d84b740039d48a1954c852b3ccfe6cbef58f8b0
NaoyaSugiyama1 <21911198ns@tama.ac.jp>
2022-03-23 23:10:53 +0900

    別のブランチから:カメラと近くなるまで茎や綿毛を全て非表示にする
    DandelionModelchange.csに関して、以前は綿毛部分のモデルの入れ替えをして処理落ちを回避していた。
    この部分をモデルの差し替えではなく、最初から茎などのすべてのモデルを非表示にして、カメラと近くなったものから表示していくように変更した。(別のブランチから)

---
+++ dandelion/application-video/Assets/DandelionModels/Scripts/DandelionModelchange2.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DandelionModelchange2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Camera;
+    public GameObject HeadModel;
+    public GameObject Stem;
+    public GameObject Core;
+    public float DistanceCamera;
+    bool Head = false;
+    Transform Cameratransform;
+    Transform Dandeliontransform;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //カメラの座標
+        Cameratransform = Camera.transform;
+        Vector3 Camerapos = Cameratransform.position;
+
+        //たんぽぽの座標
+        Dandeliontransform = this.transform;
+        Vector3 Dandelionpos = Dandeliontransform.position;
+
+        //カメラとたんぽぽ間の距離
+        DistanceCamera = Dandelionpos.z - Camerapos.z;
+
+        //距離が一定以上近くなったら簡易版のモデルを非表示にして、処理が重いモデルを表示する
+        if (DistanceCamera < 30.0f)
+        {
+            if (Head == false)
+            {
+                Core.SetActive(true);
+                Stem.SetActive(true);
+                HeadModel.SetActive(true);
+                Head = true;
+            }
+
+        }
+    }
+}
